ae2c872735c9c5f487b59eb5c1c3df9904f5f55a
qchan09 <87214012+qchan09@users.noreply.github.com>
2021-07-13 21:38:23 +0900

    Add files via upload

--- grepla.js
+++ grepla.js
@@ -1,28 +1,40 @@
-$(function(){
+$(function () {
 
-    $("#menu-icon").click(function(){
-        $(this).toggleClass("open");
-        $("#header-nav").fadeToggle(300);
-    });
+    
+        $("#menu-icon").click(function () {
+            $(this).toggleClass("open");
+            $("#header-nav").fadeToggle(300);
+        });
 
-    $(".header-list").click(function(){
-        $("#header-nav").hide();
-        $("#menu-icon").removeClass("open")
-    });
+        $(".header-list").click(function () {
+            $("#header-nav").hide();
+            $("#menu-icon").removeClass("open")
+        });
+
+        $(window).resize(function() {
+            //リサイズされたときの処理
+            if (window.matchMedia("(min-width: 768px)").matches) {
+                $("#header-nav").show();
+            }
+        });
+        
 
     $("#mainvisiual").hide().fadeIn(1000);
-    
+
     $(window).scroll(function () {
         var wHeight = $(window).height();
         var scrollAmount = $(window).scrollTop();
         $('.container').each(function () {
             var targetPosition = $(this).offset().top;
-            if(scrollAmount > targetPosition - wHeight + 150) {
+            if (scrollAmount > targetPosition - wHeight + 150) {
                 $(this).addClass("fadein");
             }
         });
     });
-    
+
+
+
+
 
 
 });
