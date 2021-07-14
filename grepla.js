1be068b3a9f7c9e0b9e83776d13b471da9a4cc4c
qchan09 <87214012+qchan09@users.noreply.github.com>
2021-07-14 21:55:44 +0900

    Add files via upload

--- grepla.js
+++ grepla.js
@@ -3,22 +3,33 @@ $(function () {
     
         $("#menu-icon").click(function () {
             $(this).toggleClass("open");
-            $("#header-nav").fadeToggle(300);
+            $("#header-nav").toggleClass("sp_open");
         });
 
         $(".header-list").click(function () {
-            $("#header-nav").hide();
-            $("#menu-icon").removeClass("open")
+            $("#menu-icon").toggleClass("open");
+            $("#header-nav").toggleClass("sp_open");
         });
 
-        $(window).resize(function() {
-            //リサイズされたときの処理
-            if (window.matchMedia("(min-width: 768px)").matches) {
-                $("#header-nav").show();
-            }else{
-                $("#header-nav").hide();
-            }
-        });
+        // $("#menu-icon").click(function () {
+        //     $(this).toggleClass("open");
+        //     $("#header-nav").fadeToggle(300);
+        // });
+
+        // $(".header-list").click(function () {
+        //     $("#header-nav").hide();
+        //     // $("#header-nav").toggleClass("close");
+        //     $("#menu-icon").removeClass("open")
+        // });
+
+        // $(window).resize(function() {
+        //     //リサイズされたときの処理
+        //     if (window.matchMedia("(min-width: 768px)").matches) {
+        //         $("#header-nav").show();
+        //     }else{
+        //         $("#header-nav").hide();
+        //     }
+        // });
         
 
     $("#mainvisiual").hide().fadeIn(1000);
